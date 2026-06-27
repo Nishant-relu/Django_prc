9974ead0710ad24b3f9ffde4b46cfcdca1f78326
Nishant-relu <nishantpratapsingh@reluconsultancy.in>
2026-06-27 15:30:21 +0530

    Product view set

--- store/views.py
+++ store/views.py
@@ -5,36 +5,26 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
+from rest_framework.viewsets import ModelViewSet
 from .models import Product, Collection
 from .serializers import ProductSerializer, CollectionSerializer
 from django.db.models import Count
 
-class ProductList(ListCreateAPIView):
 
-    queryset = Product.objects.select_related('collection').all()
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
     serializer_class = ProductSerializer
-
-    # def get_queryset(self):
-    #     return Product.objects.select_related('collection').all()
-    
-    # def get_serializer(self, *args, **kwargs):
-    #     return ProductSerializer
     
     def get_serializer_context(self):
         return {'request': self.request}
-
-class ProductDetail(RetrieveUpdateDestroyAPIView):
-
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
-
+    
     def delete(self, request, pk):
         product = self.get_object(id)
         if product.orderitems.count() > 0:
             return Response({'error': "Product cannot be deleted because linked to the order item"}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
         product.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
-    
+        
 
 # =================================================Collection======================================================#
 
