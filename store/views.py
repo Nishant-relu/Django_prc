5d8a455ca3424a8116e3c200d39fc9e922ddad10
Nishant-relu <nishantpratapsingh@reluconsultancy.in>
2026-06-27 17:12:29 +0530

    Add Review model and serializer; implement ReviewViewSet

--- store/views.py
+++ store/views.py
@@ -3,10 +3,10 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
-from .models import Collection, Product
-from .serializers import CollectionSerializer, ProductSerializer
+from .models import Collection, Product, OrderItem, Review
+from .serializers import CollectionSerializer, ProductSerializer, ReviewSerializer
 
-
+# ================================Product==========================================
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
@@ -15,16 +15,16 @@ class ProductViewSet(ModelViewSet):
         return {'request': self.request}
 
     def destroy(self, request, *args, **kwargs):
-        product = self.get_object()
-        if product.orderitems.count() > 0:
+
+        if OrderItem.objects.filter(product_id=kwargs['pk']).count() > 0:
             return Response(
                 {'error': 'Product cannot be deleted because it is linked to an order item.'},
                 status=status.HTTP_405_METHOD_NOT_ALLOWED,
             )
-        product.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        
+        return super().destroy(request, *args, **kwargs)
 
-
+# ===============================Collection==================================================
 class CollectionViewSet(ModelViewSet):
     queryset = Collection.objects.annotate(product_count=Count('product')).all()
     serializer_class = CollectionSerializer
@@ -35,3 +35,8 @@ class CollectionViewSet(ModelViewSet):
             return Response({'error': 'Collection cannot be deleted because it contains products.'}, status=status.HTTP_403_FORBIDDEN)
         collection.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+    
+# ================================Review====================================================
+class ReviewViewSet(ModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
